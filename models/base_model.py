765de19091175016802771f852fd74f8e51c6ad4
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-05 09:33:34 +0100

    Added method to generate dictionary representation of an instance

--- models/base_model.py
+++ models/base_model.py
@@ -13,9 +13,11 @@ class BaseModel():
             if 'id' in kwargs:
                 self.id = kwargs['id']
             if 'created_at' in kwargs:
-                self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                self.created_at = datetime.strptime(kwargs['created_at'],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
             if 'updated_at' in kwargs:
-                self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                self.updated_at = datetime.strptime(kwargs['updated_at'],
+                                                    '%Y-%m-%dT%H:%M:%S.%f')
         self.id = str(uuid4())
         self.created_at = datetime.utcnow()
         self.updated_at = datetime.utcnow()
