6249c2a73378ca1318bf148d7c1f695145458d8e
ken2201 <ndayishimiyeken22@gmail.com>
2024-03-05 14:59:06 +0100

    tasq 6

---
+++ console.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF command to exit the program"""
+        print()
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty input line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
