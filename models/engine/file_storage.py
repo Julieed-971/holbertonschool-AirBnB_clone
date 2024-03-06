9a59a67f035d79dd9574d09d35c22a380761c477
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-06 14:03:20 +0100

    Improved comment

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -49,8 +49,8 @@ class FileStorage():
                 if class_name in dict_module:
                     get_module = dict_module[class_name]
 
-                # Retrieve the class object corresponding to the class name from the module
-                new_model = getattr(get_module, class_name)
+                    # Retrieve the class object corresponding to the class name from the module
+                    new_model = getattr(get_module, class_name)
 
                 # Instantiate the class object with attributes extracted from the JSON data and store the instance in __objects
                 FileStorage.__objects[key] = new_model(**dict_attr)
