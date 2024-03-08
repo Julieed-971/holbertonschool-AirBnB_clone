041f7c0dd03ddce8853836040f703530f0a1afa3
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-08 22:06:41 +0100

    Fixed class attributes

--- models/user.py
+++ models/user.py
@@ -6,10 +6,8 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """Class to create a user"""
-    def __init__(self, *args, **kwargs):
-        """Instantiate a user instance"""
-        super().__init__(*args, **kwargs)
-        self.email = str()
-        self.password = str()
-        self.first_name = str()
-        self.last_name = str()
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
