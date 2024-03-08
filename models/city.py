84b2de5a556c1499a187dac93050e3678b99f9b7
Julieed-971 <julie.dedieu1@gmail.com>
2024-03-08 23:23:46 +0100

    Fixed inheritance

--- models/city.py
+++ models/city.py
@@ -2,11 +2,10 @@
 """Class to create a city"""
 
 from models.base_model import BaseModel
-from models.state import State
 
 
 class City(BaseModel):
     """Class to create a city"""
 
-    state_id = BaseModel.id
+    state_id = ""
     name = ""
